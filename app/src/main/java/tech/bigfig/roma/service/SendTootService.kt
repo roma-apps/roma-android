FWGS/Husky

Create polls (#1452)
* add AddPollDialog

* add support for pleroma poll options

* add PollPreviewView

* add Poll support to drafts

* add license header, cleanup

* rename drawable files to correct size

* fix tests

* fix bug with Poll having wrong duration after delete&redraft

* add input validation

* grey out poll button when its disabled

* code cleanup & small improvements

app/src/main/java/com/keylesspalace/tusky/service/SendTootService.kt
@@ -22,6 +22,8 @@ import com.keylesspalace.tusky.db.AccountEntity
 import com.keylesspalace.tusky.db.AccountManager
 import com.keylesspalace.tusky.db.AppDatabase
 import com.keylesspalace.tusky.di.Injectable
+import com.keylesspalace.tusky.entity.NewPoll
+import com.keylesspalace.tusky.entity.NewStatus
 import com.keylesspalace.tusky.entity.Status
 import com.keylesspalace.tusky.network.MastodonApi
 import com.keylesspalace.tusky.util.SaveTootHelper
@@ -131,16 +133,21 @@ class SendTootService : Service(), Injectable {
 
         tootToSend.retries++
 
-        val sendCall = mastodonApi.createStatus(
-                "Bearer " + account.accessToken,
-                account.domain,
+        val newStatus = NewStatus(
                 tootToSend.text,
-                tootToSend.inReplyToId,
                 tootToSend.warningText,
+                tootToSend.inReplyToId,
                 tootToSend.visibility,
                 tootToSend.sensitive,
                 tootToSend.mediaIds,
-                tootToSend.idempotencyKey
+                tootToSend.poll
+        )
+
+        val sendCall = mastodonApi.createStatus(
+                "Bearer " + account.accessToken,
+                account.domain,
+                tootToSend.idempotencyKey,
+                newStatus
         )
 
 
@@ -243,7 +250,8 @@ class SendTootService : Service(), Injectable {
                 toot.inReplyToId,
                 toot.replyingStatusContent,
                 toot.replyingStatusAuthorUsername,
-                Status.Visibility.byString(toot.visibility))
+                Status.Visibility.byString(toot.visibility),
+                toot.poll)
     }
 
     private fun cancelSendingIntent(tootId: Int): PendingIntent {
@@ -277,6 +285,7 @@ class SendTootService : Service(), Injectable {
                            mediaUris: List<Uri>,
                            mediaDescriptions: List<String>,
                            inReplyToId: String?,
+                           poll: NewPoll?,
                            replyingStatusContent: String?,
                            replyingStatusAuthorUsername: String?,
                            savedJsonUrls: String?,
@@ -295,6 +304,7 @@ class SendTootService : Service(), Injectable {
                     mediaUris.map { it.toString() },
                     mediaDescriptions,
                     inReplyToId,
+                    poll,
                     replyingStatusContent,
                     replyingStatusAuthorUsername,
                     savedJsonUrls,
@@ -337,6 +347,7 @@ data class TootToSend(val text: String,
                       val mediaUris: List<String>,
                       val mediaDescriptions: List<String>,
                       val inReplyToId: String?,
+                      val poll: NewPoll?,
                       val replyingStatusContent: String?,
                       val replyingStatusAuthorUsername: String?,
                       val savedJsonUrls: String?,

app/src/main/java/com/keylesspalace/tusky/util/SaveTootHelper.java
@@ -17,6 +17,7 @@ import com.google.gson.reflect.TypeToken;
 import com.keylesspalace.tusky.BuildConfig;
 import com.keylesspalace.tusky.db.TootDao;
 import com.keylesspalace.tusky.db.TootEntity;
+import com.keylesspalace.tusky.entity.NewPoll;
 import com.keylesspalace.tusky.entity.Status;
 
 import java.io.File;
@@ -41,17 +42,18 @@ public final class SaveTootHelper {
 
     @SuppressLint("StaticFieldLeak")
     public boolean saveToot(@NonNull String content,
-                             @NonNull String contentWarning,
-                             @Nullable String savedJsonUrls,
-                             @NonNull List<String> mediaUris,
-                             @NonNull List<String> mediaDescriptions,
-                             int savedTootUid,
-                             @Nullable String inReplyToId,
-                             @Nullable String replyingStatusContent,
-                             @Nullable String replyingStatusAuthorUsername,
-                             @NonNull Status.Visibility statusVisibility) {
+                            @NonNull String contentWarning,
+                            @Nullable String savedJsonUrls,
+                            @NonNull List<String> mediaUris,
+                            @NonNull List<String> mediaDescriptions,
+                            int savedTootUid,
+                            @Nullable String inReplyToId,
+                            @Nullable String replyingStatusContent,
+                            @Nullable String replyingStatusAuthorUsername,
+                            @NonNull Status.Visibility statusVisibility,
+                            @Nullable NewPoll poll) {
 
-        if (TextUtils.isEmpty(content) && mediaUris.isEmpty()) {
+        if (TextUtils.isEmpty(content) && mediaUris.isEmpty() && poll == null) {
             return false;
         }
 
@@ -86,7 +88,8 @@ public final class SaveTootHelper {
                 inReplyToId,
                 replyingStatusContent,
                 replyingStatusAuthorUsername,
-                statusVisibility);
+                statusVisibility,
+                poll);
 
         new AsyncTask<Void, Void, Void>() {
             @Override

roma-apps/roma-android

app/src/main/java/tech/bigfig/roma/service/SendTootService.kt
@@ -22,15 +22,17 @@ import tech.bigfig.roma.db.AccountEntity
 import tech.bigfig.roma.db.AccountManager
 import tech.bigfig.roma.db.AppDatabase
 import tech.bigfig.roma.di.Injectable
+import tech.bigfig.roma.entity.NewPoll
+import tech.bigfig.roma.entity.NewStatus
 import tech.bigfig.roma.entity.Status
 import tech.bigfig.roma.network.MastodonApi
 import tech.bigfig.roma.util.SaveTootHelper
+import tech.bigfig.roma.util.randomAlphanumericString
 import dagger.android.AndroidInjection
 import kotlinx.android.parcel.Parcelize
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import tech.bigfig.roma.util.randomAlphanumericString
 import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.TimeUnit
@@ -131,16 +133,21 @@ class SendTootService : Service(), Injectable {
 
         tootToSend.retries++
 
-        val sendCall = mastodonApi.createStatus(
-                "Bearer " + account.accessToken,
-                account.domain,
+        val newStatus = NewStatus(
                 tootToSend.text,
-                tootToSend.inReplyToId,
                 tootToSend.warningText,
+                tootToSend.inReplyToId,
                 tootToSend.visibility,
                 tootToSend.sensitive,
                 tootToSend.mediaIds,
-                tootToSend.idempotencyKey
+                tootToSend.poll
+        )
+
+        val sendCall = mastodonApi.createStatus(
+                "Bearer " + account.accessToken,
+                account.domain,
+                tootToSend.idempotencyKey,
+                newStatus
         )
 
 
@@ -243,7 +250,8 @@ class SendTootService : Service(), Injectable {
                 toot.inReplyToId,
                 toot.replyingStatusContent,
                 toot.replyingStatusAuthorUsername,
-                Status.Visibility.byString(toot.visibility))
+                Status.Visibility.byString(toot.visibility),
+                toot.poll)
     }
 
     private fun cancelSendingIntent(tootId: Int): PendingIntent {
@@ -277,6 +285,7 @@ class SendTootService : Service(), Injectable {
                            mediaUris: List<Uri>,
                            mediaDescriptions: List<String>,
                            inReplyToId: String?,
+                           poll: NewPoll?,
                            replyingStatusContent: String?,
                            replyingStatusAuthorUsername: String?,
                            savedJsonUrls: String?,
@@ -295,6 +304,7 @@ class SendTootService : Service(), Injectable {
                     mediaUris.map { it.toString() },
                     mediaDescriptions,
                     inReplyToId,
+                    poll,
                     replyingStatusContent,
                     replyingStatusAuthorUsername,
                     savedJsonUrls,
@@ -337,6 +347,7 @@ data class TootToSend(val text: String,
                       val mediaUris: List<String>,
                       val mediaDescriptions: List<String>,
                       val inReplyToId: String?,
+                      val poll: NewPoll?,
                       val replyingStatusContent: String?,
                       val replyingStatusAuthorUsername: String?,
                       val savedJsonUrls: String?,

app/src/main/java/tech/bigfig/roma/util/SaveTootHelper.java
@@ -17,6 +17,7 @@ import com.google.gson.reflect.TypeToken;
 import tech.bigfig.roma.BuildConfig;
 import tech.bigfig.roma.db.TootDao;
 import tech.bigfig.roma.db.TootEntity;
+import tech.bigfig.roma.entity.NewPoll;
 import tech.bigfig.roma.entity.Status;
 
 import java.io.File;
@@ -41,17 +42,18 @@ public final class SaveTootHelper {
 
     @SuppressLint("StaticFieldLeak")
     public boolean saveToot(@NonNull String content,
-                             @NonNull String contentWarning,
-                             @Nullable String savedJsonUrls,
-                             @NonNull List<String> mediaUris,
-                             @NonNull List<String> mediaDescriptions,
-                             int savedTootUid,
-                             @Nullable String inReplyToId,
-                             @Nullable String replyingStatusContent,
-                             @Nullable String replyingStatusAuthorUsername,
-                             @NonNull Status.Visibility statusVisibility) {
+                            @NonNull String contentWarning,
+                            @Nullable String savedJsonUrls,
+                            @NonNull List<String> mediaUris,
+                            @NonNull List<String> mediaDescriptions,
+                            int savedTootUid,
+                            @Nullable String inReplyToId,
+                            @Nullable String replyingStatusContent,
+                            @Nullable String replyingStatusAuthorUsername,
+                            @NonNull Status.Visibility statusVisibility,
+                            @Nullable NewPoll poll) {
 
-        if (TextUtils.isEmpty(content) && mediaUris.isEmpty()) {
+        if (TextUtils.isEmpty(content) && mediaUris.isEmpty() && poll == null) {
             return false;
         }
 
@@ -86,7 +88,8 @@ public final class SaveTootHelper {
                 inReplyToId,
                 replyingStatusContent,
                 replyingStatusAuthorUsername,
-                statusVisibility);
+                statusVisibility,
+                poll);
 
         new AsyncTask<Void, Void, Void>() {
             @Override